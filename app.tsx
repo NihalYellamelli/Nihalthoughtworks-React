8deb1ca863f962a4ac84484c791ae4c2dcd8660a
NihalYellamelli <nihalyellamelli@gmail.com>
2024-09-18 15:47:10 +0530

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./app.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Event Scheduler</h1>
+      <h2></h2>
+    </div>
+  );
+}
